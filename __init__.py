59e5e3b9329e3cf39c310ab10bcf9402d26a5710
IndyJoeA <joe.alloway@gmail.com>
2017-07-16 13:24:27 -0400

    HTTP protocol and set power

--- __init__.py
+++ __init__.py
@@ -5,26 +5,34 @@ import httplib2
 from flask import request
 
 @cbpi.actor
-class WIFISocket(ActorBase):
+class HTTPActor(ActorBase):
 
-    a_url = Property.Text("Server Address", configurable=True, default_value="http://<ipaddress>:<port>", description="Address of the controller")
+    a_url = Property.Text("Controller Address", configurable=True, default_value="http://<ipaddress>:<port>", description="Address of the controller. Do not add a trailing slash (ex: http://192.168.0.10)")
     b_on = Property.Text("On Command", configurable=True, default_value="control?cmd=GPIO,<pin>,1", description="Command to turn actor on")
     c_off = Property.Text("Off Command", configurable=True, default_value="control?cmd=GPIO,<pin>,0", description="Command to turn actor off")
-    d_pwm = Property.Text("PWM Command", configurable=True, default_value="control?cmd=PWM,<pin>,<level>", description="Command to set actor power level, if it supports PWM")
+    d_pwm = Property.Text("PWM Command", configurable=True, default_value="control?cmd=PWM,<pin>,", description="Command to set actor power level. Power level will be added to the end of the command. If device does not support PWM, make this field blank.")
     
-    def send(self, command, value=None):
+    power = 100
+    
+    def send(self, command):
         try:
             h = httplib2.Http(".cache")
-            headers = {'content-type': 'application/x-www-form-urlencoded'}
             ## Sending http command ""
-            (resp_headers, content) = h.request("%s/%s" % (self.a_url, command), "GET",  body=command, headers=headers)
+            (resp, content) = h.request("%s/%s" % (self.a_url, command), "GET", headers={'cache-control':'no-cache'})
         except Exception as e:
             self.api.app.logger.error("FAILED to switch HTTP actor: %s/%s" % (self.a_url, command))
 
     def on(self, power=None):
         self.send(self.b_on)
         if power is not None:
-            self.send(self.d_pwm, power)
+            self.set_power(power)
 
     def off(self):
         self.send(self.c_off)
+        
+    def set_power(self, power):
+        if power is not None and self.d_pwm is not None and self.d_pwm:
+            if power != self.power:
+                self.power = int(power)
+                self.send("%s%s" % (self.d_pwm, power))
+
